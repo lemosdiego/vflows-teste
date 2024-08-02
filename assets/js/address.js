3bdd5db908405ab9094cedaa2ecf9404a9ad1573
Washington <washington.contato1327@gmail.com>
2024-08-02 11:38:36 -0300

    Adicionando auto complete do endereço

---
+++ assets/js/address.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  function getAddressFields() {
+    return $('input[data-autocomplete="address"]');
+  }
+
+  function disabledAddressFields(disabled) {
+    getAddressFields().each(function () {
+      $(this).attr('disabled', disabled);
+    });
+  }
+
+  function resetAddressFields() {
+    getAddressFields().each(function () {
+      $(this).val('');
+    });
+  }
+
+  $('#input-cep').on('blur', function () {
+    if (this.checkValidity()) {
+      // TODO: sanitizar zipcode
+      const zipcode = $(this).val();
+
+      resetAddressFields();
+      disabledAddressFields(true);
+
+      $.ajax({
+        url: `https://viacep.com.br/ws/${zipcode}/json/`,
+        type: 'GET',
+        success: function (response) {
+          $('#input-endereco').val(response.logradouro);
+          $('#input-bairro').val(response.bairro);
+          $('#input-municipio').val(response.localidade);
+          $('#input-estado').val(response.uf);
+          disabledAddressFields(false);
+        },
+        error: function () {
+          // TODO: Adicionar mensagem de erro
+          disabledAddressFields(false);
+        }
+      });
+    }
+  });
+});
