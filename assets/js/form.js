e425b69ff0dfe9f864badff1082dfc9133c35f04
Washington <washington.contato1327@gmail.com>
2024-08-02 18:38:54 -0300

    Formatando campos de usuário e exibindo no log

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,6 +2,18 @@ $(document).ready(function () {
   $("#company-form").on("submit", function (event) {
     event.preventDefault();
     FLUIGC.loading(window).show();
-    console.log($(this).serialize());
+
+    const data = $(this)
+      .serializeArray()
+      .reduce(function (field, item) {
+        field[item.name] = item.value;
+        return field;
+      }, {});
+
+    console.log(data);
+
+    $(this)[0].reset();
+
+    FLUIGC.loading(window).hide();
   });
 });
