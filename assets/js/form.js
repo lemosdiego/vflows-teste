f07a30ea9caaf406bef5ddf954e44363435166b9
Washington <washington.contato1327@gmail.com>
2024-08-02 22:13:02 -0300

    Adicionando os produtos no formulário

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,6 +5,28 @@ $(document).ready(function () {
     event.preventDefault();
 
     FLUIGC.loading(window).show();
+    const products = $("[data-product]")
+      .map(function () {
+        const product = $(this)
+          .find("input[data-product-name], select[data-product-name]")
+          .map(function () {
+            const name = $(this).data("product-name");
+            const value = $(this).val();
+            return {
+              name,
+              value,
+            };
+          })
+          .get()
+          .reduce((field, item) => {
+            field[item.name] = item.value;
+            return field;
+          }, {});
+        return product;
+      })
+      .get();
+    console.log(products);
+
     const attachments = FLUIGC.sessionStorage.getItem(STORAGE_NAME) || [];
 
     const formData = $(this)
@@ -16,6 +38,7 @@ $(document).ready(function () {
 
     const data = {
       ...formData,
+      produtos: products,
       anexos: attachments,
     };
 
