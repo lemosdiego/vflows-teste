f6067ebd44bd59a61cbe851d9fc6da91dd9f983a
Washington <washington.contato1327@gmail.com>
2024-08-02 21:26:31 -0300

    Adicionou os anexos na session storage

--- assets/js/attachments.js
+++ assets/js/attachments.js
@@ -1,12 +1,25 @@
 $(document).ready(function () {
+  const STORAGE_NAME = "attachments";
+  FLUIGC.sessionStorage.clear();
+
   $("#input-attachments").on("change", function (event) {
     const file = event.target.files[0];
 
-    $("#no-attachment").addClass("fs-display-none");
-    const attachmentPanel = $("[data-attachment]").first().clone(true);
+    if (file) {
+      const attachments = FLUIGC.sessionStorage.getItem(STORAGE_NAME) || [];
 
-    attachmentPanel.find("span.lead").text(file.name);
-    attachmentPanel.removeClass("fs-display-none");
-    attachmentPanel.insertBefore($(this).parents(".fs-cursor-pointer"));
+      attachments.push({
+        indice: attachments.length + 1,
+        nomeArquivo: file.name,
+      });
+
+      FLUIGC.sessionStorage.setItem(STORAGE_NAME, attachments);
+      $("#no-attachment").addClass("fs-display-none");
+      const attachmentPanel = $("[data-attachment]").first().clone(true);
+
+      attachmentPanel.find("span.lead").text(file.name);
+      attachmentPanel.removeClass("fs-display-none");
+      attachmentPanel.insertBefore($(this).parents(".fs-cursor-pointer"));
+    }
   });
 });
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,18 +1,28 @@
 $(document).ready(function () {
+  const STORAGE_NAME = "attachments";
+
   $("#company-form").on("submit", function (event) {
     event.preventDefault();
-    FLUIGC.loading(window).show();
 
-    const data = $(this)
+    FLUIGC.loading(window).show();
+    const attachments = FLUIGC.sessionStorage.getItem(STORAGE_NAME) || [];
+
+    const formData = $(this)
       .serializeArray()
       .reduce(function (field, item) {
         field[item.name] = item.value;
         return field;
       }, {});
 
-    console.log(data);
+    const data = {
+      ...formData,
+      anexos: attachments,
+    };
+
+    console.log(JSON.stringify(data));
 
     $(this)[0].reset();
+    FLUIGC.sessionStorage.clear();
 
     FLUIGC.loading(window).hide();
   });
